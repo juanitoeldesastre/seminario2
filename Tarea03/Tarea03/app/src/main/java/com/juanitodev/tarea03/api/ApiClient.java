19eb1cadd5457641e7f52482eff2854edec328cb
juanitoeldesastre <juanitoeldevastador@gmail.com>
2025-04-08 11:00:19 -0500

    Modificar URL local en ApiClient

--- Tarea03/Tarea03/app/src/main/java/com/juanitodev/tarea03/api/ApiClient.java
+++ Tarea03/Tarea03/app/src/main/java/com/juanitodev/tarea03/api/ApiClient.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "https://api.finco.com/";
+    private static final String BASE_URL = "http://10.0.2.2:5000/";
 
     public static Retrofit getClient() {
         if (retrofit == null) {
